4dda3771a8017313d5dca855d97c04625b5cef1b
Vidar Tonaas Fauske <vidartf@gmail.com>
2016-08-01 15:19:17 +0200

    Make loader names explicit

--- nbdime/webapp/webpack.config.js
+++ nbdime/webapp/webpack.config.js
@@ -12,15 +12,15 @@ module.exports = {
       { test: /\.css$/, loader: 'style-loader!css-loader' },
       { test: /\.json$/, loader: 'json-loader' },
       { test: /\.ts$/, loader: 'awesome-typescript-loader' },
-      { test: /\.html$/, loader: 'file' },
+      { test: /\.html$/, loader: 'file-loader' },
       // jquery-ui loads some images
-      { test: /\.(jpg|png|gif)$/, loader: 'file' },
+      { test: /\.(jpg|png|gif)$/, loader: 'file-loader' },
       // required to load font-awesome
-      { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff' },
-      { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff' },
-      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream' },
-      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file' },
-      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml' }
+      { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff' },
+      { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff' },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=application/octet-stream' },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader' },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=image/svg+xml' }
     ],
     preLoaders: [
       // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
