47ccd45f54977b1f8d51864c77d3e2de9986a02d
Vidar Tonaas Fauske <vidartf@gmail.com>
2016-08-11 19:38:01 +0200

    Array/path utils

--- nbdime/webapp/src/util.ts
+++ nbdime/webapp/src/util.ts
@@ -33,3 +33,84 @@ export function deepCopy(obj) {
   }
   return obj;
 }
+
+/**
+ * Shallow copy routine for objects
+ */
+export
+function shallowCopy(orginal) {
+    // First create an empty object with
+    // same prototype of our original source
+    var clone = Object.create(Object.getPrototypeOf(orginal));
+
+    for (var k in orginal) {
+        // copy each property into the clone
+        Object.defineProperty(clone, k,
+            Object.getOwnPropertyDescriptor(orginal, k)
+        );
+    }
+    return clone;
+}
+
+export
+function arraysEqual(a: any[], b: any[]) {
+  if (a === b) {
+    return true;
+  }
+  if (a === null || b === null) {
+    return false;
+  }
+  if (a.length !== b.length) {
+    return false;
+  }
+  for (var i = 0; i < a.length; ++i) {
+    if (a[i] !== b[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+
+/**
+ * Find the shared common starting sequence in two arrays
+ */
+export
+function findSharedPrefix(a: any[], b: any[]): any[] {
+  if (a === b) {
+    return a.slice();
+  }
+  if (a === null || b === null) {
+    return null;
+  }
+  let i = 0;
+  for (; i < Math.min(a.length, b.length); ++i) {
+    if (a[i] !== b[i]) {
+      break;
+    }
+  }
+  return a.slice(0, i);
+}
+
+/**
+ * Check whether `parent` is contained within the start of `sub`
+ */
+export
+function isPrefixArray(parent: any[], sub: any[]): boolean {
+  if (parent === sub) {
+    return true;
+  }
+  if (parent === null || parent.length === 0) {
+    return true;
+  }
+  if (sub === null || parent.length > sub.length) {
+    return false;
+  }
+  let i = 0;
+  for (; i < sub.length; ++i) {
+    if (parent[i] !== sub[i]) {
+      return false;
+    }
+  }
+  return true;
+}
