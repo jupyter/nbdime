056ad50dbec68d79a30c325f2572eeabd5c0be66
Vidar Tonaas Fauske <vidartf@gmail.com>
2016-09-26 18:38:21 +0200

    Cleanup

--- nbdime-web/webpack.test.config.js
+++ nbdime-web/webpack.test.config.js
@@ -14,45 +14,20 @@ module.exports = {
   devtool: 'inline-source-map',
   module: {
     loaders: [
-      /**
-       * Enable inline source maps for code coverage report.
-       *
-       * See project repository for details / configuration reference:
-       * https://github.com/s-panferov/awesome-typescript-loader
-       */
-      {
-          test: /\.ts$/,
-          loader: 'awesome-typescript-loader'
-      },
       { test: /\.css$/, loader: 'style-loader!css-loader' },
       { test: /\.json$/, loader: 'json-loader' },
-      { test: /\.md$/, loader: 'raw-loader'},
-      { test: /\.html$/, loader: "file?name=[name].[ext]" },
-      { test: /\.ipynb$/, loader: 'json-loader' },
+      { test: /\.ts$/, loader: 'awesome-typescript-loader' },
+      { test: /\.html$/, loader: 'file-loader' },
       // jquery-ui loads some images
-      { test: /\.(jpg|png|gif)$/, loader: 'file' },
+      { test: /\.(jpg|png|gif)$/, loader: 'file-loader' },
       // required to load font-awesome
-      { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff' },
-      { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff' },
-      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream' },
-      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file' },
-      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml' }
-    ],
-    /*preLoaders: [
-      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-      { test: /\.js$/, loader: "source-map-loader" }
+      { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff' },
+      { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff' },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=application/octet-stream' },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader' },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=image/svg+xml' }
     ]
-    postLoaders: [
-      //Instruments TS source files for subsequent code coverage.
-      {
-        test: /\.ts$/,
-        loader: 'istanbul-instrumenter-loader',
-        exclude: [
-          'node_modules',
-          /\.(spec|d)\.ts$/
-        ]
-      }
-    ]*/
+
   },
   resolve: {
     // Add '.ts' and '.tsx' as resolvable extensions.
