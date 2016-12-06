9d6f54cbad79f9027075e5b79ade2515a633dffc
Martin Sandve Alnæs <martinal@simula.no>
2016-12-06 09:42:57 +0100

    Let nbdime mergetool take filename arguments.

--- nbdime/__main__.py
+++ nbdime/__main__.py
@@ -9,6 +9,7 @@ from __future__ import print_function
 import sys
 from subprocess import call
 
+import nbdime
 from ._version import __version__
 
 try:
@@ -23,6 +24,25 @@ HELP_MESSAGE_VERBOSE = ("Usage: nbdime [OPTIONS]\n\n"
                        "          nbdime show -h\n"
                        "          nbdime merge-web\n\n"
                        "Documentation: https://nbdime.readthedocs.io" % ", ".join(COMMANDS))
+
+
+def main_mergetool(args):
+    if not which('git'):
+        sys.exit("Cannot use \"nbdime mergetool\" alias as git is not preset on path")
+    to_call = 'git mergetool --tool=nbdimeweb'.split()
+
+    if args:
+        paths = [a for a in args if a.endswith(".ipynb")]
+        if len(paths) != len(args):
+            nbdime.log.warning("Skipping given filenames without .ipynb extension: {}".format([a for a in args if a not in paths]))
+    else:
+        paths = ["*.ipynb"]
+    to_call.extend(paths)
+
+    nbdime.log.info("Calling 'git mergetool --tool=nbdimeweb' on files {}".format(paths))
+
+    return call(to_call)
+
 
 def main_dispatch(args=None):
     if args is None:
@@ -44,10 +64,7 @@ def main_dispatch(args=None):
     elif cmd == "merge-web":
         from nbdime.webapp.nbmergeweb import main
     elif cmd == 'mergetool':
-        if not which('git'):
-            sys.exit("Cannot use \"nbdime mergetool\" alias as git is not preset on path")
-        to_call = 'git mergetool --tool=nbdimeweb *.ipynb'.split()
-        return call(to_call)
+        main = main_mergetool
     else:
         if cmd == '--version':
             sys.exit(__version__)
