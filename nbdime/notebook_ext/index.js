282ab67765f844583bda04b89db6aa6c0aacdb0a
Vidar Tonaas Fauske <vidartf@gmail.com>
2017-07-05 13:52:09 +0200

    NB ext: Conditional registration of git diff action
    Only show the gitt nbdiff button if open notebook is actually in a git
    repository.

--- nbdime/notebook_ext/index.js
+++ nbdime/notebook_ext/index.js
@@ -56,32 +56,65 @@ define([
         nbDiffView('git-difftool', base, '');
     };
 
+    var isGit = function (path) {
+        var url = window.location.origin + '/' + path_join(Jupyter.notebook.base_url, 'nbdime', 'api', 'isgit');
+        var request = {
+            data: JSON.stringify({ 'path': Jupyter.notebook.notebook_path }),
+            method: 'POST',
+        };
+        return utils.promising_ajax(url, request);
+    };
 
-    var load_ipython_extension = function() {
+    var register = function (isGit) {
         var prefix = 'nbdime';
 
+        // Register checkpoint action
         var checkpointAction = Jupyter.actions.register({
             icon: 'fa-clock-o',
             help: 'Display nbdiff from checkpoint to currently saved version',
             handler : nbCheckpointDiffView
         }, 'diff-notebook-checkpoint', prefix);
 
-        var gitAction = Jupyter.actions.register({
-            icon: 'fa-git',
-            help: 'Display nbdiff from git HEAD to currently saved version',
-            handler : nbGitDiffView
-        }, 'diff-notebook-git', prefix);
+        if (isGit) {
+            // Register git action
+            var gitAction = Jupyter.actions.register({
+                icon: 'fa-git',
+                help: 'Display nbdiff from git HEAD to currently saved version',
+                handler : nbGitDiffView
+            }, 'diff-notebook-git', prefix);
 
-        var btn_group = Jupyter.toolbar.add_buttons_group([{
-            action: checkpointAction,
-            label: 'nbdiff',
-        }, {
-            action: gitAction,
-            label: 'nbdiff',
-        }]);
+            // Add both buttons, with label on git button
+            var btn_group = Jupyter.toolbar.add_buttons_group([
+                checkpointAction,
+            {
+                action: gitAction,
+                label: 'nbdiff',
+            }]);
 
+            // Tooltip for git button:
+            btn_group.children(':last-child').attr('title', Jupyter.actions.get(gitAction).help);
+        } else {
+            // Add only checkpoint button, with label on it
+            var btn_group = Jupyter.toolbar.add_buttons_group([{
+                action: checkpointAction,
+                label: 'nbdiff',
+            }]);
+        }
+
+        // Tooltip for checkpoint button:
         btn_group.children(':first-child').attr('title', Jupyter.actions.get(checkpointAction).help);
-        btn_group.children(':last-child').attr('title', Jupyter.actions.get(gitAction).help);
+    };
+
+    var load_ipython_extension = function() {
+
+        var promise = isGit(Jupyter.notebook.path);
+        promise.then((data) => {
+            var isGit = data['is_git'];
+            register(isGit);
+        }, (error) => {
+            // Assume that we don't have git
+            register(false);
+        });
     };
 
     return {
