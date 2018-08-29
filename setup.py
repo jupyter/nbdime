039c821a988bf4393ec7c6179a6a78771daf91ca
Vidar Tonaas Fauske <vidartf@gmail.com>
2018-08-29 14:17:03 +0200

    Cleanup setup metadata

--- setup.py
+++ setup.py
@@ -92,10 +92,10 @@ setup_args = dict(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Framework :: Jupyter',
     ],
 )
