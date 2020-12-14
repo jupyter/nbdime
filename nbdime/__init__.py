d3dde51c24cd11b0d414874877af96d5d2746906
Alex Bozarth <ajbozart@us.ibm.com>
2020-12-14 14:37:50 -0800

    reenable nbserver extension

--- nbdime/__init__.py
+++ nbdime/__init__.py
@@ -12,16 +12,22 @@ from .patching import patch, patch_notebook
 from .merging import merge_notebooks, decide_merge, apply_decisions
 
 
-def _load_jupyter_server_extension(nb_server_app):
+def load_jupyter_server_extension(nb_server_app):
     # Wrap this here to avoid pulling in webapp in a normal run
     from .webapp.nb_server_extension import _load_jupyter_server_extension
     _load_jupyter_server_extension(nb_server_app)
 
 
-def _jupyter_server_extension_points():
+_load_jupyter_server_extension = load_jupyter_server_extension
+
+
+def _jupyter_server_extension_paths():
     return [{
         "module": "nbdime"
     }]
+
+
+_jupyter_server_extension_points = _jupyter_server_extension_paths
 
 
 def _jupyter_nbextension_paths():
@@ -40,5 +46,6 @@ __all__ = [
     "diff", "diff_notebooks",
     "patch", "patch_notebook",
     "decide_merge", "merge_notebooks", "apply_decisions",
+    "load_jupyter_server_extension",
     "_load_jupyter_server_extension",
     ]
