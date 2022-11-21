5c87e8627316f8cc694754956b1874327bf436a2
Cory Gwin <gwincr11@github.com>
2022-11-21 21:39:57 +0000

    link in the jupyter lab dev env

--- .devcontainer/scripts/setup.sh
+++ .devcontainer/scripts/setup.sh
@@ -3,6 +3,13 @@
 pip install .
 python setup.py build
 
-sudo mkdir /usr/local/share/jupyter
-sudo chown vscode /usr/local/share/jupyter
-jupyter labextension develop . --overwrite
+# sudo mkdir /usr/local/share/jupyter
+# sudo chown vscode /usr/local/share/jupyter
+# jupyter labextension develop . --overwrite --no-build
+jupyter server extension enable nbdime # if developing for jupyter lab or nbclassic
+
+jupyter nbextension install --py nbdime --sys-prefix [--sym-link]
+jupyter nbextension enable --py nbdime --sys-prefix
+
+jupyter labextension link ./packages/nbdime --no-build
+jupyter labextension install ./packages/labextension
