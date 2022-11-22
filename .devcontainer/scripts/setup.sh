a245196ce3561153b573e4ec5d8fd20619edd9a9
Cory Gwin <gwincr11@github.com>
2022-11-22 18:57:30 +0000

    add some watch scripts

--- .devcontainer/scripts/setup.sh
+++ .devcontainer/scripts/setup.sh
@@ -7,9 +7,4 @@ python setup.py build
 # sudo chown vscode /usr/local/share/jupyter
 # jupyter labextension develop . --overwrite --no-build
 jupyter server extension enable nbdime # if developing for jupyter lab or nbclassic
-
-jupyter nbextension install --py nbdime --sys-prefix [--sym-link]
-jupyter nbextension enable --py nbdime --sys-prefix
-
-jupyter labextension link ./packages/nbdime --no-build
-jupyter labextension install ./packages/labextension
+jupyter labextension develop . --overwrite
---
+++ scripts/watch-webapp.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+cd packages/webapp
+npm run watch
