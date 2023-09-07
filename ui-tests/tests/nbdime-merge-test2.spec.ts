89f35b969bff43788d91cb3aeadf64a8b2183b24
Frédéric Collonval <fcollonval@gmail.com>
2023-09-07 11:09:40 +0200

    Improve integration tests

--- ui-tests/tests/nbdime-merge-test2.spec.ts
+++ ui-tests/tests/nbdime-merge-test2.spec.ts
@@ -1,6 +1,6 @@
 import { expect, test } from '@playwright/test';
 
-test.beforeEach(  async ({ page }) => {
+test.beforeEach(async ({ page }) => {
 
   await page.goto('http://localhost:41000/merge');
   await page.locator('#merge-local').fill('data/merge_test2/left.ipynb');
@@ -11,23 +11,23 @@ test.beforeEach(  async ({ page }) => {
 
 /* notebooks of same length with 0 conflict*/
 test.describe('merge test2 ', () => {
-test('take a snapshot at opening', async ({ page }) => {
-  await expect.soft(page.getByText('➭')).toHaveCount(12)
-  expect(await page.locator('#main').screenshot()).toMatchSnapshot();
-});
+  test('take a snapshot at opening', async ({ page }) => {
+    await expect.soft(page.getByText('➭')).toHaveCount(12)
+    expect(await page.locator('#main').screenshot()).toMatchSnapshot();
+  });
 
-test('choose left version',  async ({ page }) => {
-  await page.locator('div:nth-child(2) > .jp-Merge-gutter-picker').first().click();
-  expect(await page.locator('#main').screenshot()).toMatchSnapshot();
-})
+  test('choose left version', async ({ page }) => {
+    await page.locator('div:nth-child(2) > .jp-Merge-gutter-picker').first().click();
+    expect(await page.locator('#main').screenshot()).toMatchSnapshot();
+  })
 
-test('choose central version',  async ({ page }) => {
-  await page.locator('div').filter({ hasText: /^➭➭➭$/ }).locator('div').nth(3).click();
-  expect(await page.locator('#main').screenshot()).toMatchSnapshot();
-})
+  test('choose central version', async ({ page }) => {
+    await page.locator('div').filter({ hasText: /^➭➭➭$/ }).locator('div').nth(3).click();
+    expect(await page.locator('#main').screenshot()).toMatchSnapshot();
+  })
 
-test('choose right version',  async ({ page }) => {
-  await page.locator('div:nth-child(3) > .cm-editor > .cm-scroller > .cm-gutters > div:nth-child(2) > div:nth-child(2) > .jp-Merge-gutter-picker').click();
-  expect(await page.locator('#main').screenshot()).toMatchSnapshot();
-})
+  test('choose right version', async ({ page }) => {
+    await page.locator('div:nth-child(3) > .cm-editor > .cm-scroller > .cm-gutters > div:nth-child(2) > div:nth-child(2) > .jp-Merge-gutter-picker').click();
+    expect(await page.locator('#main').screenshot()).toMatchSnapshot();
+  })
 })
