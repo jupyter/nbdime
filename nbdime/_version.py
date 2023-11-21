89548a7f688c31d86138371eb610c74e4810486a
Frédéric Collonval <fcollonval@users.noreply.github.com>
2023-11-21 09:38:01 +0100

    Fix version parsing (#739)
    * Fix version parsing
    
    * Avoid setting releaselevel to final as much as possible

--- nbdime/_version.py
+++ nbdime/_version.py
@@ -10,14 +10,21 @@ _specifier_ = {"a": "alpha", "b": "beta", "rc": "candidate", "": "final"}
 
 __version__ = "4.0.0"
 
-parser = re.compile(r"^(?P<major>\d+)\.(?P<minor>\d+)\.(?P<micro>\d+)((?P<releaselevel>a|b|rc)(?P<serial>\d+))?$")
+parser = re.compile(
+    r"^(?P<major>\d+)\.(?P<minor>\d+)\.(?P<micro>\d+)((?P<releaselevel>[A-z]+)(?P<serial>\d+))?$"
+)
 
 parsed_version = parser.match(__version__)
 groups = parsed_version.groupdict()
+
+release_level = groups.get("releaselevel", "") or ""
+
 version_info = VersionInfo(
     int(groups["major"]),
     int(groups["minor"]),
     int(groups["micro"]),
-    _specifier_[groups.get("releaselevel", "")],
+    # This will be whatever word is set to ensure `final`
+    # is only set when releaselevel pattern is not found
+    _specifier_.get(release_level, release_level),
     groups.get("serial", ""),
 )
