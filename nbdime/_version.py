0adbe192effb703935b0343282a9e72bc111e0d8
Frédéric Collonval <fcollonval@users.noreply.github.com>
2023-11-21 09:13:22 +0100

    Avoid setting releaselevel to final as much as possible

--- nbdime/_version.py
+++ nbdime/_version.py
@@ -6,19 +6,25 @@ from collections import namedtuple
 
 VersionInfo = namedtuple("VersionInfo", ["major", "minor", "micro", "releaselevel", "serial"])
 
-_specifier_ = {"a": "alpha", "b": "beta", "rc": "candidate"}
+_specifier_ = {"a": "alpha", "b": "beta", "rc": "candidate", "": "final"}
 
 __version__ = "4.0.0"
 
-parser = re.compile(r"^(?P<major>\d+)\.(?P<minor>\d+)\.(?P<micro>\d+)((?P<releaselevel>a|b|rc)(?P<serial>\d+))?$")
+parser = re.compile(
+    r"^(?P<major>\d+)\.(?P<minor>\d+)\.(?P<micro>\d+)((?P<releaselevel>[A-z]+)(?P<serial>\d+))?$"
+)
 
 parsed_version = parser.match(__version__)
 groups = parsed_version.groupdict()
+
+release_level = groups.get("releaselevel", "") or ""
 
 version_info = VersionInfo(
     int(groups["major"]),
     int(groups["minor"]),
     int(groups["micro"]),
-    _specifier_.get(groups.get("releaselevel", ""), "final"),
+    # This will be whatever word is set to ensure `final`
+    # is only set when releaselevel pattern is not found
+    _specifier_.get(release_level, release_level),
     groups.get("serial", ""),
 )
